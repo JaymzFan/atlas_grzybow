f3a4ba531788fb9712ea83252762052dbdb8affe
amkowa <adamk700@gmail.com>
2021-12-08 02:10:55 +0100

    testing heroku depl

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from dashboard import create_app
 import os
-print(os.environ.get("PORT"))
-port = int(os.environ.get("PORT", 5000))
 app = create_app()
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0')
