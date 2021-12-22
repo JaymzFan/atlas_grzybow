54a27abe3d229f52d2bc943220b9f9090ef3e64c
amkowa <adamk700@gmail.com>
2021-12-22 16:56:58 +0100

    schemat głównej strony lokalizacji gotowy

--- dashboard/callbacks/InteractiveMaps.py
+++ dashboard/callbacks/InteractiveMaps.py
@@ -1,23 +1,21 @@
-import dash_bootstrap_components as dbc
-import dash_html_components as html
-
-from datetime import datetime
-
-from functools import lru_cache
-
-from dash.dependencies import Input, Output, State
-
-import dash_leaflet as dl
-
-from dash.exceptions import PreventUpdate
-
-from dashboard.features.weather_api_featching import WeatherAPI
-
 import dash_table
 
+import dash_bootstrap_components as dbc
+import dash_html_components as html
 import pandas as pd
+import dash_leaflet as dl
 
 from typing import List
+
+from dash import callback_context
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import dash_leaflet.express as dlx
+
+from datetime import datetime
+from functools import lru_cache
+
+from dashboard.features.weather_api_featching import WeatherAPI
 
 
 weather_api = WeatherAPI(API_KEY='0e62530620448044eb4a76de6180486e')
@@ -28,27 +26,63 @@ def fetch_weather(today: datetime.date, lat: float, lon: float):
     return weather_api.fetch_weather(lat=lat, lon=lon)
 
 
-def render_weather_table(weather_forecast: List) -> pd.DataFrame:
-    print(weather_forecast)
-    df = pd.DataFrame.from_records(weather_forecast)
-    df = df.transpose()
-    df.reset_index()
-    df['Dane'] = df.index
-    df = df[['Dane', 0, 1, 2, 3, 4, 5, 6, 7]]
-    df.columns = df.loc['dzien']
-    df.rename(columns={'dzien': 'Dane'})
-    df = df.drop('dzien')
-    dt = dash_table.DataTable(
-            id='weather_forecast_dt',
-            columns=[{'name': i, "id": i} for i in df.columns],
-            data=df.to_dict('records'),
-            style_table={'overflowX': 'auto'},
-            style_as_list_view=True,
-            style_cell={
-                'font_size': '0.2rem'
-            }
-    )
-    return dt
+# TODO: pamiętaj o deduplikacji. Ta sama lokalizacja moze byc udostepniona i byc moja
+def fetch_locations_data():
+    demo_data = [
+        {
+            'id'        : 1,
+            'location'  : {'lng': 21, 'lat': 51, 'radius': 50},
+            'Nazwa'     : 'Lokalizacja 1',
+            'Opis'      : 'Opis 1',
+            'Grzyby'    : ['Grzyb2', 'Grzyb3', "Grzyb6"],
+            'Shared_by' : None,
+            'prywatnosc': "public"
+        },
+        {
+            'id'        : 2,
+            'location'  : {'lng': 21, 'lat': 52, 'radius': 500},
+            'Nazwa'     : 'Lokalizacja 2',
+            'Opis'      : 'Opis 2',
+            'Grzyby'    : ['Grzyb1', 'Grzyb3'],
+            'Shared_by' : None,
+            'prywatnosc': "public"
+        },
+        {
+            'id'        : 3,
+            'location'  : {'lng': 21, 'lat': 53, 'radius': 50},
+            'Nazwa'     : 'Lokalizacja 3',
+            'Opis'      : 'Opis 3',
+            'Grzyby'    : ['Grzyb1', 'Grzyb3'],
+            'Shared_by' : 'Mietek',
+            'prywatnosc': "shared_with_me"
+        },
+        {
+            'id'        : 4,
+            'location'  : {'lng': 21, 'lat': 54, 'radius': 50},
+            'Nazwa'     : 'Lokalizacja 4',
+            'Opis'      : 'Opis 4',
+            'Grzyby'    : ['Grzyb1'],
+            'Shared_by' : None,
+            'prywatnosc': "my_location"
+        }
+    ]
+    return demo_data
+
+
+def render_geojson(data):
+    data = [
+        dict(lat=x['location']['lat'], lon=x['location']['lng'], popup=x['Nazwa'], id_lokalizacji=x['id'])
+        for x in data]
+
+    return data
+
+
+def get_mushrooms_types(data):
+    typy_grzybow = []
+    for x in data:
+        typy_grzybow += x['Grzyby']
+    typy_grzybow = sorted(list(set(typy_grzybow)))
+    return typy_grzybow
 
 
 def from_datestr_to_wday_polish(datestr: str) -> str:
@@ -114,25 +148,27 @@ def register_callbacks(dash_app):
             raise PreventUpdate
         return [dl.Marker(position=click_lat_lng, children=dl.Tooltip("({:.3f}, {:.3f})".format(*click_lat_lng)))]
 
-    @dash_app.callback(Output('prognoza_pogody', 'children'),
-                       [Input('main_map', 'click_lat_lng')])
-    def get_weather_forecast(click_lat_lng):
-        if click_lat_lng is None:
-            raise PreventUpdate
-        click_lat_lng = [round(x, ndigits=1) for x in click_lat_lng]
-        today_date = datetime.now().date()
-
-        pogoda_teraz = fetch_weather(today=today_date,
-                                     lat=click_lat_lng[0],
-                                     lon=click_lat_lng[1])
-        return render_weather_table(weather_forecast=pogoda_teraz)
+    # @dash_app.callback(Output('prognoza_pogody', 'children'),
+    #                    [Input('main_map', 'click_lat_lng')])
+    # def get_weather_forecast(click_lat_lng):
+    #     if click_lat_lng is None:
+    #         raise PreventUpdate
+    #     click_lat_lng = [round(x, ndigits=1) for x in click_lat_lng]
+    #     today_date = datetime.now().date()
+    #
+    #     pogoda_teraz = fetch_weather(today=today_date,
+    #                                  lat=click_lat_lng[0],
+    #                                  lon=click_lat_lng[1])
+    #     return render_weather_table(weather_forecast=pogoda_teraz)
 
     @dash_app.callback(Output('weather_forecast_placeholder', 'children'),
-                       [Input('main_map', 'click_lat_lng')])
-    def get_weather_forecast2(click_lat_lng):
-        if click_lat_lng is None:
+                       [Input('store-current-location-data', 'data')])
+    def get_weather_forecast2(curr_data):
+        if curr_data is None:
             raise PreventUpdate
-        click_lat_lng = [round(x, ndigits=1) for x in click_lat_lng]
+
+        click_lat_lng = [round(x, ndigits=1) for x in [curr_data['location']['lat'],
+                                                       curr_data['location']['lng']]]
         today_date = datetime.now().date()
 
         pogoda_teraz = fetch_weather(today=today_date,
@@ -141,4 +177,123 @@ def register_callbacks(dash_app):
 
         return render_weather_tables(weather_forecast=pogoda_teraz)
 
+    @dash_app.callback(Output("locations_filters", "is_open"),
+                       Input('locations_show_filters', 'n_clicks'),
+                       State("locations_filters", "is_open"))
+    def pokaz_filtry(n_clicks, is_open):
+        if n_clicks:
+            return not is_open
 
+        return is_open
+
+    @dash_app.callback([Output("locations_filters_loctypes", "value"),
+                        Output('locations_filters_mushroomtypes', 'value')],
+                       Input('locations_clear_filters', 'n_clicks'),
+                       [State("locations_filters_loctypes", "options")])
+    def wyczysc_filtry(n_clicks, location_types):
+        if n_clicks == 0:
+            raise PreventUpdate
+        all_values = [x['value'] for x in location_types]
+        return list(set(all_values)), []
+
+    @dash_app.callback(Output("store-all-locations-data", "data"),
+                       Input('url', 'pathname'))
+    def load_locations_data(url):
+        if url != '/lokalizacje-przegladaj':
+            raise PreventUpdate
+
+        return fetch_locations_data()
+
+    @dash_app.callback(Output("store-filtered-locations-ids", "data"),
+                       Input('locations_apply_filters', 'n_clicks'),
+                       Input('locations_clear_filters', 'n_clicks'),
+                       [State("locations_filters_loctypes", "value"),
+                        State('locations_filters_mushroomtypes', 'value'),
+                        State('store-all-locations-data', 'data')])
+    def filter_locations(n_clicks, n_clics_clear, loc_types, mushroom_types, all_locations):
+        if n_clicks == 0 or n_clics_clear == 0:
+            raise PreventUpdate
+
+        if all_locations is None:
+            raise PreventUpdate
+
+        # filter by location type
+        filt_loc = [x for x in all_locations
+                    if x['prywatnosc'] in loc_types]
+
+        # filter by mushrooms
+        if mushroom_types:
+            for x in mushroom_types:
+                filt_loc = [_ for _ in filt_loc if x in _['Grzyby']]
+
+        return filt_loc
+
+    @dash_app.callback(Output("locations_filters_mushroomtypes", "options"),
+                       Input('store-all-locations-data', 'data'))
+    def filter_locations(all_locations):
+        if all_locations is None:
+            raise PreventUpdate
+
+        return [{'label': x, 'value': x} for x in get_mushrooms_types(all_locations)]
+
+    @dash_app.callback(Output("locations_filtered_number", "children"),
+                       Input("store-filtered-locations-ids", "data"),
+                       Input("locations_clear_filters", "n_clicks"),
+                       State('store-all-locations-data', 'data'))
+    def print_number_of_filtered_locations(filtered_locations, n_clicks, all_locations):
+
+        # determining which button was pushed
+        trigger = callback_context.triggered[0]["prop_id"].split(".")[0]
+
+        if trigger == 'locations_clear_filters' and all_locations is not None:
+            return f"Znalezionych: {len(all_locations)}"
+
+        if filtered_locations is None:
+            if all_locations is not None:
+                return f"Znalezionych: {len(all_locations)}"
+            raise PreventUpdate
+
+        return f"Znalezionych: {len(filtered_locations)}"
+
+    @dash_app.callback(Output("store-current-location-data", "data"),
+                       Input('locations_markers', 'click_feature'),
+                       State("store-all-locations-data", "data"))
+    def load_current_locations_data(clicked_loc, all_locations):
+        if clicked_loc is None:
+            return None
+
+        picked_location_id = clicked_loc['properties']['id_lokalizacji']
+        curr_location_data = [x for x in all_locations if x['id'] == picked_location_id]
+
+        return curr_location_data[0]
+
+    @dash_app.callback([Output("clicked_loc_name", 'children'),
+                        Output('clicked_loc_info', 'children'),
+                        Output('clicked_loc_mushrooms', 'children')],
+                       Input("store-current-location-data", "data"))
+    def load_current_location_card(location_data):
+        if location_data is None:
+            return None, None, None
+
+        mushrooms_list = [dbc.ListGroupItem(x) for x in location_data['Grzyby']]
+
+        return location_data['Nazwa'], location_data['Opis'], mushrooms_list
+
+    @dash_app.callback(Output("locations_markers", "data"),
+                       [Input("locations_clear_filters", "n_clicks"),
+                       Input("store-filtered-locations-ids", "data")],
+                       Input("store-all-locations-data", "data"))
+    def render_filtered_locations(n_clicks, filtered_locations, all_locations):
+
+        # determining which button was pushed
+        trigger = callback_context.triggered[0]["prop_id"].split(".")[0]
+
+        if trigger == 'locations_clear_filters' and all_locations is not None:
+            return dlx.dicts_to_geojson(render_geojson(data=all_locations))
+
+        if filtered_locations is None:
+            if all_locations is None:
+                raise PreventUpdate
+            return dlx.dicts_to_geojson(render_geojson(data=all_locations))
+
+        return dlx.dicts_to_geojson(render_geojson(data=filtered_locations))
--- dashboard/layouts/lokalizacje.py
+++ dashboard/layouts/lokalizacje.py
@@ -4,23 +4,89 @@ import dash_bootstrap_components as dbc
 
 import dash_leaflet as dl
 
-
 MAP_STYLE = {'width': '100%', 'height': '25rem'}
+
+locations_map = dl.Map([
+    dl.TileLayer(),
+    dl.GeoJSON(data=[], id='locations_markers'),
+    dl.LocateControl(options={'locateOptions'       : {'enableHighAccuracy': True},
+                              'keepCurrentZoomLevel': False,
+                              'showPopup'           : False})
+], center=[52.15, 19.7], style=MAP_STYLE, id='locations_map')
+
+clicked_loc_card = dbc.Card([
+    html.H2([], id='clicked_loc_name', className='py-1'),
+    html.Hr(),
+    dbc.CardBody(
+            [
+                html.P([], id='clicked_loc_info', className='py-1'),
+                dbc.ListGroup([], id='clicked_loc_mushrooms')
+            ]
+        ),
+])
 
 main_page = dbc.Container([
     dcc.Store(id='store_for_main_page_lokalizacje'),
-    dbc.Row(dbc.Col(html.Div(
-            [dl.Map([
-                dl.TileLayer(),
-                dl.LayerGroup(id="main_page_lokalizacje_layer"),
-                dl.LocateControl(options={'locateOptions': {'enableHighAccuracy': True},
-                                          'keepCurrentZoomLevel': False,
-                                          'showPopup': False})
-            ], id='main_map', center=[52.15, 19.7], style=MAP_STYLE)]
-    ))),
+    dcc.Store(id='store-all-locations-data'),
+    dcc.Store(id='store-filtered-locations-ids'),
+    dcc.Store(id='store-current-location-data'),
+    dbc.Row(dbc.Col(html.Div([locations_map]))),
+    dbc.Row(dbc.Col(dbc.Button('Pokaż filtry lokalizacji', id='locations_show_filters'))),
+    dbc.Offcanvas([
+        dbc.Row(dbc.Col(
+                html.Div(
+                        dbc.Button("Zastosuj filtry", id='locations_apply_filters', color='success'),
+                className='py-1'))),
+        dbc.Row(dbc.Col(
+                html.Div(
+                        dbc.Button("Wyczyść filtry", id='locations_clear_filters'),
+                        className='py-1'))),
+        dbc.Row(dbc.Col([
+            html.Div(
+                    [
+                        dbc.Label(id='locations_filtered_number'),
+                    ], className="py-4"
+            )
+        ])),
+        dbc.Row(dbc.Col([
+            html.Div(
+                    [
+                        dbc.Label("Pokaż lokalizacje:"),
+                        dbc.Checklist(
+                                options=[
+                                    {"label": "Moje", "value": 'my_location'},
+                                    {"label": "Udostępnione", "value": 'shared_with_me'},
+                                    {"label": "Publiczne", "value": 'public'},
+                                ],
+                                value=['public', 'my_location', 'shared_with_me'],
+                                id="locations_filters_loctypes",
+                                switch=True,
+                        ),
+                    ], className="py-4"
+            )
+        ])),
+        dbc.Row(dbc.Col([
+            html.Div(
+                    [
+                        dbc.Label("Musi zawierać:"),
+                        dbc.Checklist(
+                                options=[],
+                                value=[],
+                                id="locations_filters_mushroomtypes",
+                                switch=False,
+                        ),
+                    ], className="py-4"
+            )
+        ]))
+    ],
+            id='locations_filters',
+            title='Filtry lokalizacji',
+            is_open=False
+    ),
     dbc.Accordion([
         dbc.AccordionItem([
-        ], title="Wybierz lokalizację"),
+            clicked_loc_card
+        ], title="Informacje o lokalizacji"),
         dbc.AccordionItem([
             dbc.Row(id='weather_forecast_placeholder')
         ], title="Prognoza pogody"),
